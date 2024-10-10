8dba7bf3a1f66b84c5c047627a0a5e2e18d11fff
Harimisa Florent <harimisaflorent.mi@gmail.com>
2024-10-10 18:15:02 +0200

    init theme-toggle component

--- src/app/components/theme-toggle/theme-toggle.component.ts
+++ src/app/components/theme-toggle/theme-toggle.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ThemeService } from '../../services/theme/theme.service';
 
 @Component({
   selector: 'app-theme-toggle',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule],
   templateUrl: './theme-toggle.component.html',
-  styleUrl: './theme-toggle.component.scss'
+  styleUrl: './theme-toggle.component.scss',
 })
 export class ThemeToggleComponent {
+  @Output() themeToggled = new EventEmitter<boolean>();
+  isDarkTheme: boolean;
 
+  constructor(private themeService: ThemeService) {
+    this.isDarkTheme = this.themeService.isDarkTheme();
+  }
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    this.themeToggled.emit(this.isDarkTheme);
+  }
 }

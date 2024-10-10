24645d249e746d3c80fcef48e2db75487ecc9cec
Harimisa Florent <harimisaflorent.mi@gmail.com>
2024-10-10 16:29:13 +0200

    add env with openweather config

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    openWeatherApiKey: '53e8596426958a3d408214a00205ea3a',
+    openWeatherApiUrl: 'https://api.openweathermap.org/data/2.5'
+  };

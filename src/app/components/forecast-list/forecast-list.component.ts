52d789881312922a5809b85e6d1bd826e66acf0b
Harimisa Florent <harimisaflorent.mi@gmail.com>
2024-10-10 16:18:43 +0200

    init project structure with needed components and services

---
+++ src/app/components/forecast-list/forecast-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forecast-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './forecast-list.component.html',
+  styleUrl: './forecast-list.component.scss'
+})
+export class ForecastListComponent {
+
+}

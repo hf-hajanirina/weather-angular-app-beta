0e1f3a0e42b04249cb419af0caca57f9e5411de8
Harimisa Florent <harimisaflorent.mi@gmail.com>
2024-10-10 18:13:35 +0200

    init theme service

--- src/app/services/theme/theme.service.ts
+++ src/app/services/theme/theme.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ThemeService {
+  private darkTheme = false;
+  private themeSubject = new BehaviorSubject<boolean>(false);
 
-  constructor() { }
+  theme$ = this.themeSubject.asObservable();
+
+  constructor() {
+    this.initTheme();
+  }
+
+  initTheme() {
+    this.darkTheme = localStorage.getItem('darkTheme') === 'true';
+    this.setTheme(this.darkTheme);
+  }
+
+  isDarkTheme(): boolean {
+    return this.darkTheme;
+  }
+
+  setTheme(isDarkTheme: boolean) {
+    this.darkTheme = isDarkTheme;
+    this.themeSubject.next(this.darkTheme);
+    localStorage.setItem('darkTheme', this.darkTheme ? 'true' : 'false');
+    if (this.darkTheme) {
+      document.body.classList.add('dark-theme');
+    } else {
+      document.body.classList.remove('dark-theme');
+    }
+  }
+
+  toggleTheme() {
+    this.setTheme(!this.darkTheme);
+  }
 }
